a82b1543add32a1cafded4838d61a7234c42f808
Memo Chou <memochou1993@gmail.com>
2022-11-29 01:02:50 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,25 +4,35 @@ import { getFirestore } from 'firebase-admin/firestore';
 const app = initializeApp({
   credential: cert('./credentials.json'),
 });
-
 const db = getFirestore(app);
+const collection = db.collection('users');
+
+const setItem = async (key, value) => {
+  await collection.doc(key).set(value);
+};
+
+const fetchItems = async () => {
+  const items = {};
+  const snapshot = await collection.get();
+  snapshot.forEach((item) => {
+    items[item.id] = item.data();
+  });
+  return items;
+};
+
+const removeItem = async (key) => {
+  await collection.doc(key).delete();
+};
 
 (async () => {
-  const docRef = db.collection('users').doc('memochou1993');
-  await docRef.set({
+  await setItem('memochou1993', {
     name: 'Memo Chou',
     age: 18,
   });
-})();
 
-(async () => {
-  const snapshot = await db.collection('users').get();
-  snapshot.forEach((doc) => {
-    console.log(doc.id, '=>', doc.data());
-  });
-})();
+  console.table(await fetchItems());
+  
+  await removeItem('memochou1993');
 
-(async () => {
-  const res = await db.collection('users').doc('memochou1993').delete();
-  console.log(res);
+  console.table(await fetchItems());
 })();
